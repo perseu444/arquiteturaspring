ca76ea71489e382c15acb53d4fed95fdc7f6bb6d
Jader Marcelo <jadermarcelo95@gmail.com>
2025-04-21 16:29:58 -0300

    first commit

---
+++ src/test/java/io/github/perseu444/arquiteturaspring/ArquiteturaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.perseu444.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
